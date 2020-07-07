940d4ce74c79eed385de1f717a07a2cf3daa43bf
Nohossat TRAORE <nohossat.traore@gmail.com>
2020-07-07 10:41:17 +0200

    update docker config

---
+++ README.md
@@ -0,0 +1,13 @@
+# Flask Resume
+
+A simple Single Page Application with a mock resume. 
+
+The app is deployed in Heroku : https://nohossat-flask-app.herokuapp.com/
+
+## Local Installation
+
+```python
+git clone https://github.com/Nohossat/flask-resume.git
+cd flask-resume
+docker-compose up
+```
--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, url_for
 from flask import render_template
+import os
+
 app = Flask(__name__)
 
 person = {
@@ -87,7 +89,12 @@ person = {
     'interests' : ['Dance', 'Travel', 'Languages']
 }
 
-
 @app.route('/')
 def cv(person=person):
     return render_template('index.html', person=person)
+
+if __name__ == "__main__":
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
